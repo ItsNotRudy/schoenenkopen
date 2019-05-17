8d5563cd0c54d1cd3c3e4c243c1d76398c1c7117
Rudy van Sloten <rdvansloten@Rudy-MBP.local>
2019-05-17 14:54:25 +0200

    JSON from MySQL

--- get-shoes.php
+++ get-shoes.php
@@ -11,11 +11,8 @@
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$statement = $conn->prepare("SELECT JSON_OBJECT('schoen_id', schoen_id, 'schoen_prijs', schoen_prijs, 'schoen_naam', schoen_naam, 'schoen_url', schoen_url, 'schoen_desc', schoen_desc) FROM schoenen");
 		$statement->execute();
-		#$data = json_encode($statement->fetchAll(PDO::FETCH_ASSOC));
-		$data = $statement->fetchAll(PDO::FETCH_ASSOC);
-		print_r($data);
-
-		
+		$data = json_encode($statement->fetchAll(PDO::FETCH_ASSOC));	
+		echo $data;	
 	}
 	catch(PDOException $e) {
 		echo "Connection failed: " . $e->getMessage();

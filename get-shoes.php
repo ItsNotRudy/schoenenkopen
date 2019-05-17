909b583e3bec57b185664aa97e02cb067a2e1bca
Rudy van Sloten <rdvansloten@Rudy-MBP.local>
2019-05-17 12:12:32 +0200

    debug

--- get-shoes.php
+++ get-shoes.php
@@ -6,7 +6,7 @@
 	$password = $database['password'];
 
 	try {
-		$conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+		$conn = new PDO('mysql:host=$hostname;dbname=$dbname', $username, $password);
 		// set the PDO error mode to exception
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		echo "Connected successfully"; 
@@ -18,4 +18,7 @@
 	$statement = $pdo->prepare("SELECT * FROM schoenen");
 	$statement->execute();
 	$data = json_encode($statement->fetchAll(PDO::FETCH_ASSOC));
+
+	echo $conn;
+	echo $data;
 ?>

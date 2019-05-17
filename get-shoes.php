192ca282036c7d09fd71116a68a136b0d1ee4abd
Rudy van Sloten <rdvansloten@Rudy-MBP.local>
2019-05-17 12:19:31 +0200

    Add SSL

--- get-shoes.php
+++ get-shoes.php
@@ -4,9 +4,15 @@
 	$username = $database['username'];
 	$dbname = $database['dbname'];
 	$password = $database['password'];
+	$options = , array(
+		PDO::MYSQL_ATTR_SSL_KEY    =>'/etc/mysql/ssl/client-key.pem',
+		PDO::MYSQL_ATTR_SSL_CERT=>'/etc/mysql/ssl/client-cert.pem',
+		PDO::MYSQL_ATTR_SSL_CA    =>'/etc/mysql/ssl/ca-cert.pem'
+		)
+	);
 
 	try {
-		$conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+		$conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password, $options);
 		// set the PDO error mode to exception
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		echo "Connected successfully"; 

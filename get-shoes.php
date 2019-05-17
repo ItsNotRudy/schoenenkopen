6dd5d0050fbfcc1b06ba07b4375e461cb996e350
Rudy van Sloten <rdvansloten@Rudy-MBP.local>
2019-05-17 14:55:36 +0200

    JSON from MySQL

--- get-shoes.php
+++ get-shoes.php
@@ -9,7 +9,7 @@
 		$conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
 		// set the PDO error mode to exception
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$statement = $conn->prepare("SELECT JSON_OBJECT('schoen_id', schoen_id, 'schoen_prijs', schoen_prijs, 'schoen_naam', schoen_naam, 'schoen_url', schoen_url, 'schoen_desc', schoen_desc) FROM schoenen");
+		$statement = $conn->prepare("SELECT schoen_id, schoen_prijs, schoen_naam, schoen_url, schoen_desc FROM schoenen");
 		$statement->execute();
 		$data = json_encode($statement->fetchAll(PDO::FETCH_ASSOC));	
 		echo $data;	
